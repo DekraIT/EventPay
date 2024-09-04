82d31afaf778f4112b87a95a6b068bf96d3aba85
DekraIt <denni.k96@gmx.de>
2024-09-05 00:00:55 +0200

    pretty login form

--- next/src/app/login/page.tsx
+++ next/src/app/login/page.tsx
@@ -1,13 +1,16 @@
+import { Input } from '@/components/ui/input';
 import { login } from './actions';
+import { Label } from '@/components/ui/label';
+import { Button } from '@/components/ui/button';
 
 export default function LoginPage() {
   return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
+    <form className="w-100 flex flex-col gap-4 p-4">
+      <Label htmlFor="email">Email:</Label>
+      <Input id="email" name="email" type="email" required />
+      <Label htmlFor="password">Password:</Label>
+      <Input id="password" name="password" type="password" required />
+      <Button formAction={login}>Log in</Button>
       {/* <button formAction={signup}>Sign up</button> */}
     </form>
   );
